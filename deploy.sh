aee97674c818035886460839231a2218bf5418b6
Yuzo <hyzgog@gmail.com>
2014-09-30 18:43:20 +0800

    update deploy setting

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+# compile
 coffee --compile *.coffee
 coffee --compile wwwfiles/js/*.coffee
 lessc -x wwwfiles/css/style.less wwwfiles/css/style.css
 jade wwwfiles/*.jade
+
+# compress
+uglifyjs wwwfiles/js/script.js --compress -o script.js
 
 echo "The files are ready to deploy."
